68edb8727b09161ac33182c823b9e426a6b36aa4
Daniel Haendel <daniel.haendel@tbs.students.de>
2024-02-05 13:25:29 +0100

    new colums in SDS011 & DHT22

--- venv/main/models.py
+++ venv/main/models.py
@@ -4,20 +4,34 @@ from django.db import models
 
 class SDS011_3659_2022(models.Model):
     id = models.AutoField(primary_key=True)
-    pm25 = models.FloatField()
-    pm10 = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    sensor_id = models.IntegerField(default=3659)
+    sensor_type = models.CharField(max_length=6,default='SDS011')
+    location = models.IntegerField(null=True)
+    lat = models.FloatField(null=True)
+    lon = models.FloatField(null=True)
+    timestamp = models.DateTimeField()
+    p1 = models.FloatField(null=True)
+    durp1 = models.FloatField(null=True)
+    ratiop1 = models.FloatField(null=True)
+    p2 = models.FloatField(null=True)
+    durp2 = models.FloatField(null=True)
+    ratiop2 = models.FloatField(null=True)
 
     def __str__(self):
-        return f"timestamp: {self.timestamp}"
+        return f"id: {self.id}, sensor_type: {self.sensor_type}"
 
 
 class DHT22_3660_2022(models.Model):
     id = models.AutoField(primary_key=True)
-    temperature = models.FloatField()
-    humidity = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    sensor_id = models.IntegerField(default=3660)
+    sensor_type = models.CharField(max_length=6,default='DHT22')
+    location = models.IntegerField(null=True)
+    lat = models.FloatField(null=True)
+    lon = models.FloatField(null=True)
+    timestamp = models.DateTimeField(null=True)
+    temperature = models.FloatField(null=True)
+    humidity = models.FloatField(null=True)
 
     def __str__(self):
-        return f"timestamp: {self.timestamp}"
+        return f"id: {self.id}, sensor_type: {self.sensor_type}"
     
