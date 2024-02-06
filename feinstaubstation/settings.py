11ec2841c30c9c6430acd8cb97ae608a4c61915e
Daniel Haendel <daniel.haendel@tbs.students.de>
2024-02-06 09:57:04 +0100

    add html and css

--- feinstaubstation/settings.py
+++ feinstaubstation/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. 
+
+def main(request):
+    return render(request, 'main/main.html')
+
