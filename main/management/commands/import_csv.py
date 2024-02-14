5f08aaa195378fc8adc39d2508d6fb30983b99ac
Jan Zimmermann <jan.zimmermann@mait.de>
2024-02-14 14:24:32 +0100

    CSV Import - beginn SQL Export

--- main/management/commands/import_csv.py
+++ main/management/commands/import_csv.py
@@ -1,24 +1,41 @@
-from django.core.management.base import BaseCommand
-from main.models import *
-import csv
+import os
+from django.db import connection
+
+def readFiles(folder):
+    for file in os.listdir(folder):
+        #print("File:" + folder + "/" + file) Bug Check
+        readLines(folder + "/" + file)
+
+def readLines(filepfad):
+    file = open(filepfad, "r")
+    lines = file.readlines()
+    for line in lines:
+        # TODO put lines SQL
+        print(line)
+def convertData(line):  # From string to SQL Light String
+    splittet = line.split(";")
+    returnedStr = "";
 
 
-class Command(BaseCommand):
-    help = 'Importiert Daten aus einer CSV-Datei in die Datenbank'
+    if splittet[1].lower() == "dht22":
+        dht = "()"
+    elif splittet[1].lower() == "sds011":
+        sds = ""
 
-    def add_arguments(self, parser):
-        parser.add_argument('csv_file_path', type=str, help='Der Pfad zur CSV-Datei')
+    return returnedStr
+def insertIntoSQL(table, dataArray):
+    with connection.cursor() as cursor:
+        # TODO Check if exsisting
+        # TODO Insert if not Existing
 
-    def handle(self, *args, **kwargs):
-        csv_file_path = kwargs['csv_file_path']
+def checkIfExisting(table, timestamp):
+    with connection.cursor as cursor:
+        dht = "(id, sensor_id, sensor_type, location, lat, lon, timestamp, temperature, humidity)"
+        sds = "(id, sensor_id, sensor_type, location, lat, lon, timestamp, p1, durp1, ratiop1, p2, durp2, ratiop2)"
 
-        with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                Person.objects.create(
-                    name=row['Name'],
-                    alter=row['Alter'],
-                    stadt=row['Stadt']
-                )
-
-        self.stdout.write(self.style.SUCCESS('Daten erfolgreich importiert.'))
+        if table.contains("sds"):
+            cursor.execute("IF NOT EXISTING (select 1 FROM " + table + " WHERE timestamp = "+ timestamp +") BEGIN "  
+                            "INSERT INTO "+table+" " + sds + "VALUES " + "")
+        else:
+            cursor.execute("IF NOT EXISTING (select 1 FROM " + table + " WHERE timestamp = " + timestamp + ") BEGIN "
+                            "INSERT INTO " + table + " " + dht + "VALUES " + "")
